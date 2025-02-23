712fbda339003af14da089a04e3f6a115366799e
Luis Fernando Merino Saquicela <lmerino@est.ups.edu.ec>
2025-02-23 11:57:26 -0500

    Validacion para inicializar la app y activacion de logs

--- kafka-consumer/src/main/java/ec/ups/ms/kafka/config/FirebaseInitializer.java
+++ kafka-consumer/src/main/java/ec/ups/ms/kafka/config/FirebaseInitializer.java
@@ -6,21 +6,26 @@ import com.google.firebase.FirebaseOptions;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import java.io.IOException;
 
 @Service
 public class FirebaseInitializer {
 
     public FirebaseInitializer() {
+        Logger.getLogger("com.google").setLevel(Level.ALL);
         try {
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(new ClassPathResource("firebase-config.json").getInputStream()))
                     .build();
-
             if (FirebaseApp.getApps().isEmpty()) {
                 FirebaseApp.initializeApp(options);
+                System.out.println("Firebase inicializado correctamente.");
+            } else {
+                System.out.println("Firebase ya estaba inicializado.");
             }
-
             System.out.println("Firebase inicializado correctamente.");
         } catch (IOException e) {
             throw new RuntimeException("Error al inicializar Firebase", e);
