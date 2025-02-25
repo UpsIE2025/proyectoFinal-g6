010baca0397594d678eec673e3e58e3c920d3257
Kevin <kevinabastidas@gmail.com>
2025-02-25 10:02:13 -0500

    addAuth0

---
+++ Auth0/Auth0/Controllers/TokenAuth0Controller.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http;
+
+namespace Auth0.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenAuth0Controller : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenAuth0Controller(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult ValidateToken([FromBody] string token)
+        {
+           
+            if (string.IsNullOrEmpty(token)) {
+                return BadRequest(new { issuccess = false , message = "Token no proporcionado" });
+            }
+            try
+            {
+                var handler = new JwtSecurityTokenHandler();
+                var jsonToken = handler.ReadToken(token) as JwtSecurityToken;
+
+                if (jsonToken == null)
+                {
+                    return Unauthorized(new { issuccess = false, message = "Token inválido" });
+                }
+
+                var expiration = jsonToken.ValidTo;
+                if (expiration < DateTime.UtcNow)
+                {
+                    return Unauthorized(new { issuccess = false, message = "Token expirado" });
+                }
+
+                return Ok(new { issuccess = true, message = "Token válido" });
+
+            }
+            catch (Exception ex)
+            {
+                return Unauthorized(new { issuccess = false, message = "Token inválido" });
+            }
+        }
+    }
+}
---
+++ Auth0/Auth0/Models/Auth0.cs
@@ -0,0 +1,10 @@
+﻿namespace Auth0.Models
+{
+    public class Auth0
+    {
+        public string client_id {  get; set; }
+        public string client_secret { get; set; }
+        public string audience { get; set; }
+        public string grant_type { get; set; }
+    }
+}
